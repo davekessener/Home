80fb3437e08ac318bec7277443961625e0641161
Dave Kessener <davekessener@gmail.com>
2017-12-21 22:26:24 +0100

    fuck web-design

--- app/routes/audiobooks.rb
+++ app/routes/audiobooks.rb
@@ -6,3 +6,13 @@ get '/audiobooks/index' do
 	slim :'audiobooks/index'
 end
 
+get '/audiobooks/franchise/:id' do |id|
+	f = Franchise.find(id.to_i)
+
+	if f
+		slim :'audiobooks/franchise', locals: { franchise: f }
+	else
+		status 404
+	end
+end
+
